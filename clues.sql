de27cab8c6387840811f3c876be32e597adcc904
Ashley Laisure <ashley.laisure@gmail.com>
2025-05-13 19:09:39 -0400

    lab completed

--- clues.sql
+++ clues.sql
@@ -1,32 +1,35 @@
+-- Run the SQL file in the terminal with psql -d world -f clues.sql
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
 
 -- Write SQL query here
-SELECT * FROM countries WHERE region = 'Southern Europe';
-SELECT * FROM countries WHERE population = (SELECT MIN(population) FROM countries WHERE region = "Southern Europe");
 SELECT code  FROM countries WHERE population = (SELECT MIN(population) FROM countries WHERE region = 'Southern Europe');
+-- VAT
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
 SELECT language FROM countrylanguages WHERE countrycode = 'VAT';
+-- Italian
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
 SELECT * FROM countrylanguages WHERE language = 'Italian' AND percentage = 100;
+-- SMR         | Italian  | t          |        100
 SELECT countrycode FROM countrylanguages WHERE language = 'Italian' AND percentage = 100;
-
+-- SMR
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
 SELECT name FROM countries WHERE code = 'SMR';
+-- San Marino
 SELECT * FROM cities WHERE countrycode = 'SMR' and name != 'San Marino';
 -- 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
-SELECT * FROM cities WHERE name LIKE 'Serr%';
+SELECT * FROM cities WHERE name LIKE 'Serr%' and name != 'Serravalle';
 -- 265 | Serra      | BRA         | Espírito Santo    |     302666
 
 SELECT name FROM countries WHERE code = 'BRA';
